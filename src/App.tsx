a7d0f97b1236d9e724bee3205f5d1b82b8b10d0e
javad-azizi <mohammadjavadazizi1381@gmail.com>
2025-05-28 20:48:01 +0330

    Create Students page with term and class filtering

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ function App() {
             </PrivateRoute>
           }
         />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/login\" replace />} />
       </Routes>
     </Router>
   );
