dbfeabb64d062a654ad6a88a2fd1f520a24d2fc5
javad-azizi <mohammadjavadazizi1381@gmail.com>
2025-05-28 10:48:52 +0330

    Create Classes page with term selection and modals

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ function App() {
             </PrivateRoute>
           }
         />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/login\" replace />} />
       </Routes>
     </Router>
   );
