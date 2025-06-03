cfb56c523002bc65a1d2677bd7f968908edba201
javad-azizi <mohammadjavadazizi1381@gmail.com>
2025-06-03 15:36:29 +0330

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -121,7 +121,7 @@ function App() {
             </PrivateRoute>
           }
         />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/login\" replace />} />
       </Routes>
     </Router>
   );
